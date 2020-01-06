2a3a6cfc4afb56c828a91335fb7fe3a397d11bc8
pingchun <pingchun@mogujie.com>
2020-01-06 22:32:08 +0800

    update

--- src/main/java/com/xk/bean/Person.java
+++ src/main/java/com/xk/bean/Person.java
@@ -2,20 +2,16 @@ package com.xk.bean;
 
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 /**
  * @author pingchun@meili-inc.com
  * @since 2019/7/27
  */
-@Component("person")
 public class Person {
 
-    @Value("${person.name}")
     private String name;
 
-    @Value("${person.age}")
     private int age;
 
     public Person(){
